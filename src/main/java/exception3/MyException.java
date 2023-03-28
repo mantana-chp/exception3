e5b5ebfe2aae6e0c5e52de174127bdc9e38f9afd
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2023-03-28 16:34:50 +0700

    exception part2

--- src/main/java/exception3/MyException.java
+++ src/main/java/exception3/MyException.java
@@ -1,8 +1,8 @@
 package exception3;
 
 public class MyException extends Exception {
-    public MyException(String msg) {
-        super(msg);
+    public MyException() {
+        super();
     }
     public MyException(String msg) {
         super(msg);
